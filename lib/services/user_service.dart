4a5df409a4ca14cf59af5f64b6c214155faf7bab
Angelo van Osch <Angelo.van.osch@hotmail.com>
2024-06-26 02:18:49 +0200

    added get all users endpoint

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,12 +1,19 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/user.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class UserService {
   final String apiUrl = "http://127.0.0.1:8080/users";
+  final _storage = const FlutterSecureStorage();
 
   Future<List<User>> fetchUsers() async {
-    final response = await http.get(Uri.parse(apiUrl));
+    final token = await _storage.read(key: 'jwt');
+
+    final response = await http.get(
+      Uri.parse(apiUrl),
+      headers: {'Authorization': 'Bearer $token'},
+    );
 
     if (response.statusCode == 200) {
       List<dynamic> body = jsonDecode(response.body);
